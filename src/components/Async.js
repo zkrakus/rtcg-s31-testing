4694c4e65a282cc83dc57a38f2b338c448770c5a
zkrakus <zkrakus@gmail.com>
2025-09-06 10:42:08 -0400

    test:added a quick fetch.

---
+++ src/components/Async.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const Async = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((respone) => respone.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <h2>{post.title}</h2>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Async;
