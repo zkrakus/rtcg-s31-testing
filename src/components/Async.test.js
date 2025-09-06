3e2278f859eeac6fe918fdfa88a36ed1ebf9cf9a
zkrakus <zkrakus@gmail.com>
2025-09-06 10:47:35 -0400

    test:Some light mocking.

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders posts if requests succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: 'p1', title: 'First Post'}]
+    });
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole("listitem", {}, {});
